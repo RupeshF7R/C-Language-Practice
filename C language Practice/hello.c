9ae9a074d30546396e183ba99bce1e1541084c83
onlyrupeshh <rupeshkumarsharma283@gmail.com>
2023-11-11 15:09:24 +0530

    predict the answer

--- C language Practice/hello.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("hello world i am become coder soon !\n hello rupesh");
-    
-    return 0;
-}
---
+++ C language Practice/predict the answer.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("hello world i am become coder soon !\n hello rupesh\n");
+    printf("nn\n\nnn\n");
+    printf("nn/n/nnn/n");
+    
+    return 0;
+}
