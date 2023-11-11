e176570fab228b7df60eadee8bf70e0649aeb9ec
onlyrupeshh <rupeshkumarsharma283@gmail.com>
2023-11-11 15:41:28 +0530

    variables in c

--- new.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("news.c");
-    return 0;
-}
---
+++ variables.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+
+    printf("rupesh Sharma");
+    int x;
+    x = 6;
+    // printf("%d", x);
+    printf("\nbefore %d", x);
+    x = 9;
+    printf("\nafter %d", x);
+    x = 9+1;
+    printf("\nafter n after %d", x);
+    x = 98*3;
+    printf("\nlast final after %d", x);
+    x = 400;
+    printf("\nsuper last after %d", x);
+
+    return 0;
+}
