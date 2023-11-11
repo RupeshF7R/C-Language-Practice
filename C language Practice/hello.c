6736408c9ebf63d614819666cbc3ae9bdbab8c63
onlyrupeshh <rupeshkumarsharma283@gmail.com>
2023-11-11 15:04:36 +0530

    fix error

---
+++ C language Practice/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("hello world i am become coder soon !\n hello rupesh");
+    
+    return 0;
+}
--- C language Practice/me.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("hello world i am become coder soon !\n");
-    printf("hello Rupesh i am become coder soon !");
-}
