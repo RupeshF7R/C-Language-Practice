549a3ca1147081d896d6d032bea0e929c6f05a30
onlyrupeshh <rupeshkumarsharma283@gmail.com>
2023-11-12 10:58:49 +0530

    re practice of variables

---
+++ Re Practice/variables/vari.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    int a;
+    int b;
+    int c;
+    a = 88;
+    b = 889;
+    c = 868;
+    x = 898;
+    // Perform all arithmatic programs
+    printf("Perform all arithmatic programs");
+    printf("\n%d", x - a);
+    printf("\n%d", x - b);
+    printf("\n%d", x - c);
+    printf("\n%d", x - x + 3 * 2 + a + b + c - x * 3);
+}
