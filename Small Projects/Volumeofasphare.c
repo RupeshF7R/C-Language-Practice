44f8f64f60609e492de5d355790f108dfda7c1bd
onlyrupeshh <rupeshkumarsharma283@gmail.com>
2023-11-12 16:23:22 +0530

    make code of volume of sphare

---
+++ Small Projects/Volumeofasphare.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    // 4/3 pi r*r*r
+    int v;
+    int r;
+    printf("Welcome to Nofix Formula");
+    printf("\nEnter the Radius of spahre\n");
+    scanf("%d",&r);
+    printf("\nthe redius of sphare is %d",r);
+    printf("\nthe volume of sphare is %f",4*3.14*r*r*r/3);
+    printf("\n%f",v);
+
+
+    return 0;
+}
---
+++ data types/float.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float x = 5;
+    float y = 2;
+    float z = x / y;
+
+    printf("hello rupesh");
+    // printf("%f",z);
+    printf("%f",x/y);
+    return 0;
+}
