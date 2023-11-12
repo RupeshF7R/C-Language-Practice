40e857b170c5a60d7ba6f3deae129af8f7768c9a
onlyrupeshh <rupeshkumarsharma283@gmail.com>
2023-11-12 17:07:59 +0530

    done home work area of circle

---
+++ HomeWork/areaofcircle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    float r;
+    float aoc;
+    printf("\nwelcome to nofix formula");
+    printf("\nenter the radius of circle ");
+    scanf("%f",&r);
+    printf("the radius is %f",r);
+    printf("the final aoc is %f",3.14*r*r);
+
+
+
+
+    return 0;
+}
