8e2e04acdd29c0fad29686526e0b2d8c6fc2879b
onlyrupeshh <rupeshkumarsharma283@gmail.com>
2023-11-12 11:11:52 +0530

    fianl i made a pprogram of area of triangle our self

---
+++ Re Practice/Area of triangle/triangle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    int b;
+    int h;
+    printf("Welcome to triangle Answer\n");
+    printf("Enter the base of triangle\n");
+    scanf("%d,", &b);
+    printf("base height is %d", b);
+    printf("\nEnter the height of triangle\n");
+    scanf("%d,", &h);
+    printf("base height is %d", h);
+    printf("\nthe final answer of area of triangle is %d", b * h / 2);
+    printf("\nTHANKS FOR USING OUR FORMULAS FIXER");
+}
