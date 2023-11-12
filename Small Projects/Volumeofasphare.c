cfea00edd7156337a12ec6b19fc235802057c7c6
onlyrupeshh <rupeshkumarsharma283@gmail.com>
2023-11-12 16:39:49 +0530

    edit pi value

--- Small Projects/Volumeofasphare.c
+++ Small Projects/Volumeofasphare.c
@@ -6,11 +6,11 @@ int main()
     int r;
     printf("Welcome to Nofix Formula");
     printf("\nEnter the Radius of spahre\n");
-    scanf("%d",&r);
-    printf("\nthe redius of sphare is %d",r);
-    printf("\nthe volume of sphare is %f",4*3.14*r*r*r/3);
-    printf("\n%f",v);
-
+    scanf("%d", &r);
+    printf("\nthe redius of sphare is %d", r);
+    printf("\nthe volume of sphare is %f", 4 * 3.1489 * r * r * r / 3);
+    v = r;
+    printf("\n%f", v);
 
     return 0;
 }
