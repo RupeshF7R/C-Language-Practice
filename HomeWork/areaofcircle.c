ac4d6bc4c69878cb4ecd10c73cb18ea9eb8bfc37
onlyrupeshh <rupeshkumarsharma283@gmail.com>
2023-11-13 13:47:48 +0530

    add keywords

--- HomeWork/areaofcircle.c
+++ HomeWork/areaofcircle.c
@@ -1,15 +1,13 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     float r;
     float aoc;
     printf("\nwelcome to nofix formula");
     printf("\nenter the radius of circle ");
-    scanf("%f",&r);
-    printf("the radius is %f",r);
-    printf("the final aoc is %f",3.14*r*r);
-
-
-
+    scanf("%f", &r);
+    printf("the radius is %f", r);
+    printf("the final aoc is %f", 3.14 * r * r);
 
     return 0;
 }
