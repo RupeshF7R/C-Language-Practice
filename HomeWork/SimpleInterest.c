dbbd8e68940319f83c8006b7ad620d87c71b4aca
onlyrupeshh <rupeshkumarsharma283@gmail.com>
2023-11-13 14:27:14 +0530

    make simple interst codes

---
+++ HomeWork/SimpleInterest.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    float p, r, t,si;
+
+    printf("Welcome to NoFix Emi Loans");
+    printf("\nThis Is Simple Interest Counter app");
+    printf("\nEnter Your Priciple Amount\n");
+    scanf("%f", &p);
+    printf("\nEnter Your Interest Rate\n");
+    scanf("%f", &r);
+    printf("\nEnter Your time (in Years)\n");
+    scanf("%f", &t);
+    si = p*r*t/100+p;
+    printf("\nThis Amount Pay Extra %f\n",si);
+
+    return 0;
+}
