381aaff1f7d71eb33aaed722ea28fce485d68bdb
onlyrupeshh <rupeshkumarsharma283@gmail.com>
2023-11-14 12:55:59 +0530

    make predict the answer in c

---
+++ HomeWork/PredictOutput.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("welcome to the nofix Predictor");
+    printf("enter the 2 values");
+    scanf("%d %d", &a, &b);
+    printf("P = %d q = %d", a, b);
+    return 0;
+}
