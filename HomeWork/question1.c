51355dcf1961931c753c22a338dd1dfe4ee62d8c
onlyrupeshh <rupeshkumarsharma283@gmail.com>
2023-11-17 15:17:53 +0530

    question 3 c file

--- HomeWork/question1.c
+++ HomeWork/question1.c
@@ -1,3 +1,10 @@
+/**
+ * This C program takes a float value as input, converts it to an integer, and then calculates the
+ * decimal part of the float value.
+ * 
+ * @ return The main function is returning an integer value of 0.
+ */
+
 #include <stdio.h>
 int main()
 {
