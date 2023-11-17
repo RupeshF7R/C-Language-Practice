bba6ccba7e530a17c13c00c92bd8262af693f4c5
onlyrupeshh <rupeshkumarsharma283@gmail.com>
2023-11-17 15:17:42 +0530

    char 1 c code

---
+++ data types/Char Data type/char1.c
@@ -0,0 +1,17 @@
+/**
+ * The above function prints the character 'h' and the string "Hello World!" to the console.
+ * 
+ * @ return The main function is returning an integer value of 0.
+ */
+
+#include <stdio.h>
+int main()
+
+{
+    char a = '!';
+    printf(" %d ", a);
+    printf("Hello World!");
+    
+    
+    return 0;
+}
