f1fea489c638ebbd535f1aa2f2032eb58b05139c
onlyrupeshh <rupeshkumarsharma283@gmail.com>
2023-11-19 19:35:16 +0530

    if else code in c

---
+++ Loops/ifelse.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b = 9;
+    printf("Enter a Number : ");
+    scanf("%d", &a);
+    if (a % 2 == 0)
+    {
+        printf("enven Number");
+
+    }
+    if (a%2!=0)
+    {
+        printf("Odd Number");
+    }
+    
+    
+    // else{
+    //     printf("odd number");
+    // }
+
+    // if (a<7)
+    // {
+    //     printf("beee cumm hai bee gandu");
+    // }else{
+    //     printf("jydaa hai");
+    // }
+    // if (a == b)
+    // {
+    //     printf("nyc guess");
+    // }
+    // else
+    // {
+    //     printf("chal bosdk");
+    // }
+
+    return 0;
+}
