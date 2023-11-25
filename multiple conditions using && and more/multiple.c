e777bb72d520ebf254d7ca28608e4e6e9d51fa26
onlyrupeshh <rupeshkumarsharma283@gmail.com>
2023-11-25 13:50:30 +0530

    question with solution

---
+++ multiple conditions using && and more/multiple.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("enter a number : ");
+    scanf("%d",&a);
+    if (a>5)
+    {
+        printf("this is bigger then 5");
+    }else
+    {
+        printf("this is smaller than 5");
+    }
+    
+    
+    return 0;
+}
